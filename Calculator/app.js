6d02ef42a4ce6ed275a0d49a0695167014532911
Muhammad-Bilal-Taghi <78375305+Muhammad-Bilal-Taghi@users.noreply.github.com>
2021-03-27 11:59:52 +0500

    Add files via upload

---
+++ Calculator/app.js
@@ -0,0 +1,20 @@
+// var a;
+// for(a=1; a<=10; a++){
+//     document.write("9"+ "x" + a + "=" + 9*a +"<br>");
+// }
+// document.write("<br>");
+// var b;
+// for(b=1; b<=10; b++){
+//     document.write("6"+ "x" + b + "=" + 6*b +"<br>");
+// }
+
+var a =prompt("Enter your value");
+for(var b=1; b<=10 ;b++)
+if(a >=1)
+{
+   document.write(a + "x" + b + "="  + a*b + "<br>");
+}
+else
+{
+   document.write("5" + "x" + b + "=" + 5*b + "<br>")
+}
